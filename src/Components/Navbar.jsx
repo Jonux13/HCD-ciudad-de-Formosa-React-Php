952639d609179c2ecc70097734e88ae21ac9dffb
jonux <josejbogarin13@gmail.com>
2024-11-28 11:47:26 -0300

    feature: se le agrega al menu finde sesiones ordinarias el año 2024

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -382,7 +382,7 @@ function Navbar() {
                     className="nav-link scrollto"
                     onClick={closeMobileNav}
                   >
-                    Fin Sesiones Ordinarias
+                    Fin Sesiones Ordinarias 2024
                   </NavLink>
                 </li>
                 <li>
