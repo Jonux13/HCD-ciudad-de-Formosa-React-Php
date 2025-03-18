7ade927f25388a2e0b5a1a974a9dcbab7339340f
Jose Bogarin <josejbogarin13@gmail.com>
2025-03-18 11:32:42 -0300

    feature: cambio-sesiones y ordenanzas

--- data/itemsSesionesOrdinarias2025.js
+++ data/itemsSesionesOrdinarias2025.js
@@ -1,12 +1,25 @@
-export const itemsSesionesOrdinarias2025 = [
-{
-  date: "05-03-25",
-  files: [
-    "5-3-1.webp",
-    "5-3-2.webp",
-    "5-3-3.webp",
-    "5-3-4.webp",
-    "5-3-5.webp",
-  ]
-},
+export const itemsSesionesOrdinarias2025 = 
+[
+  {
+    date: "05-03-25",
+    files: [
+      "5-3-1.webp",
+      "5-3-2.webp",
+      "5-3-3.webp",
+      "5-3-4.webp",
+      "5-3-5.webp",
+    ]
+  },
+  {
+    date: "12-03-25",
+    files: [
+      "pdf-1.pdf",
+      "pdf-2.pdf",
+      "pdf-3.pdf",
+      "12-3-1.webp",
+      "12-3-2.webp",
+      "12-3-3.webp",
+      "12-3-4.webp",
+    ]
+  },
 ]
